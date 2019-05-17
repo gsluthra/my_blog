e07bc733911e363315e85a00c6640b81b523a68e
Gurpreet Luthra <gsluthra@gmail.com>
2019-05-17 12:17:55 +0400

    Added some explanatory messages for adding website remot

--- publish-to-github.sh
+++ publish-to-github.sh
@@ -1,5 +1,12 @@
+## Read this: http://ixti.net/software/2013/01/28/using-jekyll-plugins-on-github-pages.html
+## Explains how if you push compiled stuff to github, then there is no jekyll plugins to worry about.
 JEKYLL_ENV=production bundle exec rake site:publish
 git checkout gh-pages
+
+## Expecting a remote called "website" to exist in git
+## You can register with this command:
+## git remote add website https://github.com/gsluthra/gsluthra.github.io.git
+## Local gh-pages is pushed to master of website.
 git push website gh-pages:master --force
 git checkout master
 
